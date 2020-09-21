8cba8f6b78dfbcfba914e8125cba59b44604d249
Kaleb Thornton <krt237@msstate.edu>
2020-09-21 18:50:33 -0500

    Fixed broken tests; added registration tests

--- code/app/tests.py
+++ code/app/tests.py
@@ -44,13 +44,14 @@ class TestAuth(TestCase):
         """
 
         response = self.client.get('/register')
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
 
-    def test_submit_bad_login_form(self):
+    def test_submit_login_form_fail(self):
         """
         Tests submitting a login form with invalid credentials.
         """
 
+        # submit some invalid credentials
         response = self.client.post(
             path='/login',
             data={
@@ -59,21 +60,24 @@ class TestAuth(TestCase):
             }
         )
 
+        # validate that user is not logged in and given an error message
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Invalid username or password.")
         self.assertFalse(response.context['user'].is_authenticated)
 
-    def test_submit_good_login_form(self):
+    def test_submit_login_form_success(self):
         """
         Tests submitting a login form with valid credentials.
         """
 
+        # submit a login form with valid credentials
         response = self.client.post(
             path='/login',
             data={**self.credentials},
             follow=True
         )
 
+        # validate the user is logged in and redirected to homepage
         self.assertTrue(response.context['user'].is_authenticated)
         self.assertNotContains(response, "Invalid username or password.")
         self.assertRedirects(response, '/')
@@ -83,15 +87,62 @@ class TestAuth(TestCase):
         Tests the logout of a user.
         """
 
+        # login a user for testing
         self.client.login(**self.credentials)
 
+        # call logout
         response = self.client.get('/logout', follow=True)
 
+        # validate that the user was in-fact, logged out
         user = auth.get_user(self.client)
         self.assertFalse(user.is_authenticated)
 
+        # validate that the user was redirect to the login page with a success message
         self.assertRedirects(response, '/login')
         self.assertContains(response, 'Successfully logged out!')
+
+    def test_registration_success(self):
+        """
+        Tests successfully registering a new user.
+        """
+
+        # submit register form with new credentials
+        response = self.client.post(
+            path='/register',
+            data={
+                'username': 'some_user',
+                'password': 'some_password',
+                're_password': 'some_password'
+            },
+            follow=True
+        )
+
+        # validate the user is logged in and redirected to the homepage (inbox)
+        self.assertTrue(response.context['user'].is_authenticated)
+        self.assertRedirects(response, '/')
+
+        # validate that the user object was created
+        self.assertIsNotNone(CustomUser.objects.get(username='some_user'))
+
+    def test_registration_fail(self):
+        """
+        Tests unsuccessfully registering a new user.
+        """
+
+        # submit an invalid, already existing credential
+        response = self.client.post(
+            path='/register',
+            data={
+                'username': self.credentials['username'],
+                'password': self.credentials['password'],
+                're_password': self.credentials['password']
+            }
+        )
+
+        # validate that it rejects and gives error message
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'A user with that username already exists.')
+        self.assertFalse(response.context['user'].is_authenticated)
 
 
 class TestInbox(TestCase):
